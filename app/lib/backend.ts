9daa1fe00bb3fa29a7afbf10e1b5412b8c0fdd6b
MobSlicer152 <brambleclaw1414@gmail.com>
2025-02-24 00:59:51 -0500

    Fix backend

--- app/lib/backend.ts
+++ app/lib/backend.ts
@@ -1,6 +1,6 @@
 import { AppData } from "./context";
 
-const API_HOST = "http://74.15.101.124:42069"
+const API_HOST = "https://randomcode.dev"
 const API_ROOT = API_HOST + "/warp7api/scouting"
 const API_SEND = API_ROOT + "/add_report"
 
