7d4d011f4a22d7d8837eaff5ece12d77b6abc822
MobSlicer152 <brambleclaw1414@gmail.com>
2025-02-23 21:57:51 -0500

    Update backend

--- app/lib/backend.ts
+++ app/lib/backend.ts
@@ -1,6 +1,6 @@
 import { AppData } from "./context";
 
-const API_HOST = "http://localhost:42069"
+const API_HOST = "http://randomcode.dev:42069"
 const API_ROOT = API_HOST + "/warp7api/scouting"
 const API_SEND = API_ROOT + "/add_report"
 
@@ -14,6 +14,7 @@ export async function sendReport(data: AppData) {
         const response = await fetch(API_SEND, {
             method: "POST",
             body: json,
+            // text/plain instead of application/json so i dont have to handle OPTIONS
             headers: { "Content-Type": "text/plain; charset=UTF-8" }
         });
     } catch { };
